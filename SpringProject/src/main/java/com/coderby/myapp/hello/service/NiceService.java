bd53c86ff3c698c0c09e244449ef9799eebab2d3
LOTTE <LOTTE@LOTTE-PC>
2017-10-11 16:12:35 +0900

    [20171011] Annotation 기반 의존성 주입 - Service가 2개인 경우, 변수 이름으로 분류

---
+++ SpringProject/src/main/java/com/coderby/myapp/hello/service/NiceService.java
@@ -0,0 +1,12 @@
+package com.coderby.myapp.hello.service;
+
+public class NiceService implements IHelloService {
+
+	@Override
+	public String sayHello(String name) {
+		System.out.println("NiceService.sayHello() 메서드 실행");
+		String message = "Hello~~~ " + name;
+		return message;
+	}
+
+}
