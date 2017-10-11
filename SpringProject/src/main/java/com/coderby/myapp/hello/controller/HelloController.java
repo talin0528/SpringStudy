51c8ec6facfb4bbd5cafb9c62b51113c9a54426c
LOTTE <LOTTE@LOTTE-PC>
2017-10-11 14:41:26 +0900

    [20171011] DI 사용 코드 - 생성자 베이스

--- SpringProject/src/main/java/com/coderby/myapp/hello/controller/HelloController.java
+++ SpringProject/src/main/java/com/coderby/myapp/hello/controller/HelloController.java
@@ -1,10 +1,13 @@
 package com.coderby.myapp.hello.controller;
 
-import com.coderby.myapp.hello.service.HelloService;
 import com.coderby.myapp.hello.service.IHelloService;
 
 public class HelloController {
-	IHelloService helloService = new HelloService();
+	IHelloService helloService;
+	
+	public HelloController(IHelloService helloService) {
+		this.helloService = helloService;
+	}
 	
 	public void hello(String name) {
 		System.out.println("HelloController" + helloService.sayHello(name));
