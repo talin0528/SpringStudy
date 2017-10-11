8799e6c05e85adad72f438cbc5cd2d3ab1b6277b
LOTTE <LOTTE@LOTTE-PC>
2017-10-11 16:08:04 +0900

    [20171011] Annotation 이용한 의존성 주입

--- SpringProject/src/main/java/com/coderby/myapp/hello/controller/HelloController.java
+++ SpringProject/src/main/java/com/coderby/myapp/hello/controller/HelloController.java
@@ -1,8 +1,14 @@
 package com.coderby.myapp.hello.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
 import com.coderby.myapp.hello.service.IHelloService;
 
+@Controller
 public class HelloController {
+	
+	@Autowired
 	IHelloService helloService;
 	
 	public void setHelloService(IHelloService helloService) {
